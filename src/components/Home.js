0ba9afa1968cc13c176e5791a4818ef97c187e8c
olliebrownlow <olliebrownlow@gmail.com>
2019-09-24 13:02:06 +0100

    manages logged in state when a signed up user signs in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Registration from './auth/Registration';
+import Login from './auth/Login';
 
 export default class Home extends Component {
   constructor(props) {
@@ -18,7 +19,8 @@ export default class Home extends Component {
       <div>
         <h1>Home</h1>
         <h1>Status: {this.props.loggedInStatus}</h1>
-        <Registration handleSuccessfulAuth={this.handleSuccessfulAuth}/>
+        <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
+        <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
       </div>
     );
   }
---
+++ src/components/auth/Login.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      loginErrors: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [event.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    const { email, password } = this.state;
+
+    axios.post("http://localhost:3001/sessions", {
+      user: {
+        email: email,
+        password: password
+      }
+    },
+    // { withCredentials: true }
+  )
+  .then(response => {
+    if (response.data.logged_in) {
+      console.log("login res", response);
+      this.props.handleSuccessfulAuth(response.data);
+    }
+  })
+  .catch((err) => {
+    console.log("login error", err);
+  });
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+
+          <button type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
